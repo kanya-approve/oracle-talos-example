57860502ca8e004449ddeaa15bb53344ded6ce9c
Brian Kanya <briankanya@gmail.com>
2024-08-21 12:15:21 -0400

    update main.tf

--- modules/oci_talos_image/main.tf
+++ modules/oci_talos_image/main.tf
@@ -2,14 +2,15 @@ data "oci_objectstorage_namespace" "default_namespace" {
   compartment_id = var.compartment_id
 }
 
+
 resource "oci_core_image" "talos_amd64" {
   compartment_id = var.compartment_id
   display_name   = "Talos AMD64"
   launch_mode    = "PARAVIRTUALIZED"
 
   image_source_details {
-    bucket_name              = oci_objectstorage_bucket.custom_images_bucket.name
-    namespace_name           = oci_objectstorage_bucket.custom_images_bucket.namespace
+    bucket_name              = var.images_bucket
+    namespace_name           = data.oci_objectstorage_namespace.default_namespace
     object_name              = "oracle-amd64.oci"
     operating_system         = "Talos"
     operating_system_version = "1.7.6"
@@ -28,8 +29,8 @@ resource "oci_core_image" "talos_arm64" {
   launch_mode    = "PARAVIRTUALIZED"
 
   image_source_details {
-    bucket_name              = oci_objectstorage_bucket.custom_images_bucket.name
-    namespace_name           = oci_objectstorage_bucket.custom_images_bucket.namespace
+    bucket_name              = var.images_bucket
+    namespace_name           = data.oci_objectstorage_namespace.default_namespace
     object_name              = "oracle-amd64.oci"
     operating_system         = "Talos"
     operating_system_version = "1.7.6"
